35ea6f6f3818d16d06517ea479f09d17b362bf57
Daniel Wellner <daniel.wellner@sysart.fi>
2014-03-06 17:59:59 +0200

    this ll have to do for now, split ruby install stuff into own script, which has to be run seperately

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,31 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
 sudo apt-get update
 
 # install ruby depencencies
-sudo apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev
+sudo apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev nodejs
 
-# Install ruby using rbenv
-cd
-git clone git://github.com/sstephenson/rbenv.git .rbenv
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-exec $SHELL
 
-git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
-exec $SHELL
+# Install rbenv
+sudo -u vagrant git clone git://github.com/sstephenson/rbenv.git ~vagrant/.rbenv
+sudo -u vagrant git clone git://github.com/sstephenson/ruby-build.git ~vagrant/.rbenv/plugins/ruby-build
 
-rbenv install 2.1.1
-rbenv global 2.1.1
-
-# tell Rubygems not to install the documentation for each package locally
-echo "gem: --no-ri --no-rdoc" > ~/.gemrc
-
-# install nodejs
-sudo apt-get install -y nodejs
-
-# install rails
-gem install rails
-rbenv rehash
+echo 'export PATH="~vagrant/.rbenv/plugins/ruby-build/bin:~vagrant/.rbenv/bin:$PATH"' >> ~vagrant/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~vagrant/.bashrc
+sudo -u vagrant echo "gem: --no-ri --no-rdoc" > ~vagrant/.gemrc
 
---
+++ vagrant/install-ruby.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rbenv install 2.1.1
+rbenv global 2.1.1
+gem install rails
+rbenv rehash
