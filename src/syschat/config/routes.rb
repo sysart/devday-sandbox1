b165b4655a599004c08770413906188845a519df
Daniel Wellner <daniel.wellner@sysart.fi>
2014-03-07 18:43:20 +0200

    added a main page

--- src/syschat/config/routes.rb
+++ src/syschat/config/routes.rb
@@ -1,8 +1,9 @@
 Syschat::Application.routes.draw do
+  get "welcome/index"
   resources :messages
-
   resources :chatrooms
-
+  
+  root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
